a355aedafd7387d135358909a2bb3b59c169238b
rhenriquez94 <rodrigohenriquez94@gmail.com>
2025-04-21 23:12:02 -0400

    add venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rodrigoh/web_scraping/venv
+set -gx VIRTUAL_ENV /home/rodrigoh/Documentos/Proyectos_ETL/BeerAnalyzer/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
